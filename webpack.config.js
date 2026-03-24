68f11d346f09b9455d76dbf5ea76c5a7bef71e40
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-24 11:12:52 -0400

    Set up webpack and jest.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js", // entry point of your project
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+};
